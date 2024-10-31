99009e1482b7904089397e602cf251df92cbf6db
Draco <yanlong2944@gmail.com>
2024-10-31 11:53:26 +0800

    new file:   cmd/config/config.go init:   corba init config

---
+++ cmd/config/config.go
@@ -0,0 +1,42 @@
+/*
+Copyright © 2024 DracoYan-111 <yanlong2944@gmail.com>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/common-nighthawk/go-figure"
+	"github.com/spf13/cobra"
+)
+
+// ConfigCmd represents the config/config command
+var ConfigCmd = &cobra.Command{
+	Use:   "config",
+	Short: "Set up a configuration file for use",
+	Long:  figure.NewFigure("config", "", true).String(),
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("config/config called")
+	},
+}
+
+func init() {
+}
